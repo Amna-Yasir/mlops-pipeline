8d52e35b3053f24d2b0233ce5b8d77b3c4740026
Amna <amnayasir4321@gmail.com>
2025-05-19 11:49:22 +0500

    Fix GitHub Actions: set PYTHONPATH and fix imports for tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+
 from src.train import train_and_save_model
 
 def test_model_accuracy():
